89d094a1c7c457a5023b006414902958c11827ed
umiiii419 <umiiii419@gmail.com>
2021-08-25 06:20:43 +0000

    [Add]管理者のログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :sessions
+
+  devise_for :admin, controllers: {   registrations: 'admin/registrations',
+                                    sessions: 'admin/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

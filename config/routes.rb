bae4164d7174f82688fad36b60f0ec46c58d8b57
umiiii419 <umiiii419@gmail.com>
2021-08-25 12:48:31 +0000

    [Add]管理者トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :admin, controllers: {   registrations: 'admin/registrations',
                                     sessions: 'admin/sessions' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :admin do
+    get 'admin' => "homes#top"
+  end
 
 end

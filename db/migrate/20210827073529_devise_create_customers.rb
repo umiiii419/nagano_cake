1d3d61e78f78722854b2e4f99ec6ec892ab31f90
umiiii419 <umiiii419@gmail.com>
2021-08-27 08:08:36 +0000

    [Add]customerモデルのカラムを作成

--- db/migrate/20210827073529_devise_create_customers.rb
+++ db/migrate/20210827073529_devise_create_customers.rb
@@ -32,6 +32,16 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_active, default: true, null: false
+      t.datetime :created_at
+      t.datetime :updated_at
 
       t.timestamps null: false
     end

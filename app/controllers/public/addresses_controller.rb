a0833ae9d0f026813be0910c83636cbfc4f52d2e
umiiii419 <umiiii419@gmail.com>
2021-09-02 14:39:20 +0000

    [Add]addressのcontrollerアクションを作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,31 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+    @addresses = Address.all
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address, :customer_id)
+  end
+
 end

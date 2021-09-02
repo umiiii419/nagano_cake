e6eb3bf58704be578418ec6e25082a658f6c6509
umiiii419 <umiiii419@gmail.com>
2021-09-02 11:43:48 +0000

    [Add]性と名を連結

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def name
+    [first_name, last_name].join(' ')
+  end
+
 end

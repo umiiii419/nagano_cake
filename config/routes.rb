d998ec6f704f29a92d80834b8b6957e6707c7a71
umiiii419 <umiiii419@gmail.com>
2021-09-02 00:01:48 +0000

    [Add]顧客マイページのルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'public/homes#top'
-  get "/about" =>"public/homes#about"
-
-  scope module: :admin do
+ scope module: :admin do
     get 'admin' => "homes#top"
   end
+
+  root to: 'public/homes#top'
+  get "/about" =>"public/homes#about"
 
   devise_for :admin, controllers: { registrations: 'admin/registrations', sessions: 'admin/sessions' }
 
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :genres, except: [:new, :show, :destroy]
     resources :customers, except: [:new, :create, :destroy]
+  end
+
+  scope module: :public do
+    get "/customers/my_page", to: 'customers#show', as: :customer
+    resources :customers, only: [:edit, :update]
   end
 
 end

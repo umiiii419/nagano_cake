dc851f79086c732f784ff4a4c3fd3bfda2ccdc8d
umiiii419 <umiiii419@gmail.com>
2021-09-06 01:38:16 +0000

    [Add]ordersのコントローラとルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- scope module: :admin do
-    get 'admin' => "homes#top"
-  end
+ devise_for :admin, controllers: { registrations: 'admin/registrations', sessions: 'admin/sessions' }
+ devise_for :customers, controllers: { registrations: 'public/registrations', sessions: 'public/sessions' }
 
-  root to: 'public/homes#top'
+ root to: 'public/homes#top'
   get "/about" =>"public/homes#about"
-
-  devise_for :admin, controllers: { registrations: 'admin/registrations', sessions: 'admin/sessions' }
-
-  devise_for :customers, controllers: { registrations: 'public/registrations', sessions: 'public/sessions' }
 
   namespace :admin do
     resources :items, except: [:destroy]
     resources :genres, except: [:new, :show, :destroy]
     resources :customers, except: [:new, :create, :destroy]
+  end
+
+  scope module: :admin do
+    get 'admin' => "homes#top"
   end
 
   scope module: :public do
@@ -24,7 +23,10 @@ Rails.application.routes.draw do
     resources :items, only: [:index,:show]
     resources :addresses, except: [:new, :show]
     resources :cart_items, except: [:new, :show, :edit]
-    delete "/cart_items/destroy_all", to: 'cart_items#destroy_all', as: :cart_items_all
+    delete "/cart_items/destroy_all", to: 'cart_items#destroy_all', as: :cart_items_destroy_all
+    resources :orders, only: [:new, :create, :index, :show]
+    post "/orders/confirm", to: 'orders#confirm', as: :order_confirm
+    get "/orders/thanks", to: 'orders#thanks', as: :order_thanks
   end
 
 end

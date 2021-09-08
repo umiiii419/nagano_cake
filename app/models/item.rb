ead2e6c0c96ea4ef79b4e6a46fb9b5ab936fc299
umiiii419 <umiiii419@gmail.com>
2021-09-08 02:30:35 +0000

    [Add]OrderDetailモデルを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :genre
 
   has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
 
   def with_tax_price
       (self.price * 1.1 ).floor
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_004305) do
+ActiveRecord::Schema.define(version: 2021_09_08_014625) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -79,6 +79,16 @@ ActiveRecord::Schema.define(version: 2021_09_06_004305) do
     t.datetime "updated_at", null: false
     t.integer "genre_id", null: false
     t.index ["genre_id"], name: "index_items_on_genre_id"
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
